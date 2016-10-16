0a9893666eaffe48f047c37df444c4f812bf3a5e
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-16 13:07:44 -0700

    [server-feature] - created base routing structure

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const rootRouter = require('./router/index');
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -12,5 +13,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use('/', express.static(path.join(__dirname, '../client')));
+
+app.use('/api', rootRouter);
 
 app.listen(PORT, () => console.log("Server running on port", PORT));
