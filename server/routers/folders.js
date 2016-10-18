64d7eea0fba8477bb8e4296a7707647a053e6b98
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-17 21:41:22 -0700

    [server-add] - Created routing for folders

--- server/routers/folders.js
+++ server/routers/folders.js
@@ -1,4 +1,21 @@
-// /GET folders
-// /POST folders
-// /GET folders/:folderId   --> grab specific oflder and it contains notes
-// /POST folders/:folderId    ---> post a note to a specific folder. If no folderId given, folder would be root
+const Router = require('express').Router
+
+Router.route('/folders')
+  .get((req, res) => {
+    console.log('GET /folders')
+  })
+  .post((req, res) => {
+    console.log('POST /folders')
+  })
+
+Router.route('/folders/:folderId')
+  // get specific oflder and it contains notes
+  .get((req, res) => {
+    console.log('GET /folders:folderId')
+  })
+  // post a note to a specific folder. If no folderId given, folder would be root
+  .post((req, res) => {
+    console.log('POST /folders:folderId')
+  })
+
+module.exports = Router
