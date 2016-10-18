673f42bccbea8287a1a8ebdf9b084802b40fd4ff
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-17 21:31:53 -0700

    [add] - Standard JS module added, added basic auth routing

--- server/routers/auth.js
+++ server/routers/auth.js
@@ -1,2 +1,13 @@
-// /POST signup
-// /post signin
+const Router = require('express').Router
+
+Router.route('/signup')
+  .post((req, res) => {
+    console.log('Signup in route')
+  })
+
+Router.route('/signin')
+.post((req, res) => {
+  console.log('Sign in route')
+})
+
+module.exports
