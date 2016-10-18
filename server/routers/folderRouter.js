754051f2b520088c98cd16c87c6e5a375efac147
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-18 16:35:19 -0700

    [server-refactor] - Refactor server file structure

--- server/routers/folderRouter.js
+++ server/routers/folderRouter.js
@@ -1,35 +1,12 @@
 const folderRouter = require('express').Router()
+const folderController = require('../controllers/folderController')
 
 folderRouter.route('/')
-  // gets folders for a specific user
-  .get((req, res) => {
-    console.log('GET /folders')
-    res.status(200).json({
-      message: '/folders GET'
-    })
-  })
-  .post((req, res) => {
-    // Creates a new folder
-    console.log('POST /folders')
-    res.status(200).json({
-      message: '/folders POST'
-    })
-  })
+  .get(folderController.GET)
+  .post(folderController.POST)
 
 folderRouter.route('/:folderId')
-  // get specific oflder and it contains notes
-  .get((req, res) => {
-    console.log('GET /folders/:folderId')
-    res.status(200).json({
-      message: '/folders/:folderId GET'
-    })
-  })
-  // post a note to a specific folder. If no folderId given, folder would be root
-  .post((req, res) => {
-    console.log('POST /folders/:folderId')
-    res.status(200).json({
-      message: '/folders/:folderId POST'
-    })
-  })
+  .get(folderController.GET_FOLDERID)
+  .post(folderController.POST_FOLDERID)
 
 module.exports = folderRouter
