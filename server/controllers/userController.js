c0d1a827a18c26e8573af7dda09716c82ab78999
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-18 23:20:35 -0700

    [server-feature] - Complete authentication

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,33 @@
+const User = require('../models/user')
 const userController = {}
 
 // Sign in user
 userController.SIGNIN = (req, res) => {
-  console.log('POST /signin')
   res.status(200).json({
-    message: '/signin POST'
+    message: 'Successfully signed in user.',
+    user: req.body,
+    session: req.session
   })
 }
 
 // Create a new user
 userController.SIGNUP = (req, res) => {
-  console.log('POST /signup')
+  User.register(req.body)
+    .then((user) => {
+      res.status(200).json({
+        message: 'Successfully registered user.',
+        user: user
+      })
+    })
+    .catch((err) => {
+      res.status(500).err(err)
+    })
+}
+
+// logout user
+userController.LOGOUT = (req, res) => {
   res.status(200).json({
-    message: '/signup POST'
+    message: 'Successfully logged out user.'
   })
 }
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,3 +1,106 @@
-module.exports = {
+const Users = require('../db/db').Users
+const bcrypt = require('bcrypt')
 
+let register = (user) => {
+  // This function accepts an object with a first name, last name, email, and password
+  Users.create(user)
+    .then((result) => {
+      return result
+    })
+    .catch((err) => {
+      res.err(err)
+    })
+}
+
+let verifyPassword = (req, res, next) => {
+  Users.findOne({
+    'email': req.body.email
+  })
+  .then((result) => {
+    bcrypt.compare(req.body.password, result.password, (err, response) => {
+      if (err) {
+        res.err(err)
+      } else if (response) {
+        next()
+      } else {
+        res.status(400).send('Invalid email or password')
+      }
+    })
+  })
+  .catch((err) => {
+    res.err(err)
+  })
+}
+
+let createToken = (req, res, next) => {
+  // This function adds a token onto the user's session
+  req.session.regenerate((err) => {
+    if (err) {
+      res.err(err)
+    } else {
+      req.session.user = {
+        'email': req.body.email
+      }
+      next()
+    }
+  })
+}
+
+let verifyAuth = (req, res, next) => {
+  // This function checks to see if a session exists
+  if (req.session.user) {
+    next()
+  } else {
+    res.status(400).send('You are not logged in')
+  }
+}
+
+let destroyToken = (req, res, next) => {
+  // This function destroys the user's session
+  req.session.destroy(function (err) {
+    if (err) {
+      res.err(err)
+    } else {
+      next()
+    }
+  })
+}
+
+let hashPassword = (req, res, next) => {
+  // This function accepts a password and hashes it
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      res.err(err)
+    } else {
+      req.body.password = hash
+      next()
+    }
+  })
+}
+
+let verifyEmail = (req, res, next) => {
+  // This function accepts an email and makes sure it's unique
+  Users.findOne({
+    'email': req.body.email
+  })
+  .then((result) => {
+    if (result !== null) {
+      res.status(400).send('That email has already been used')
+    } else {
+      next()
+    }
+  })
+  .catch((err) => {
+    res.err(err)
+  })
+}
+
+module.exports = {
+  register: register,
+  verifyPassword: verifyPassword,
+  createToken: createToken,
+  verifyAuth: verifyAuth,
+  destroyToken: destroyToken,
+  hashPassword: hashPassword,
+  verifyEmail: verifyEmail
 }
