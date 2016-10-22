733138efdf8b374a5c1d7ea4ec88b2cd186dfc68
Chris Phan <cpp6d@mail.missouri.edu>
2016-10-22 16:21:45 -0700

    [client-feature]- Completed Side-Bar for all pages

--- app/components/view_folder.js
+++ app/components/view_folder.js
@@ -9,6 +9,11 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar, {muiTheme} from 'material-ui/AppBar'
 import RaisedButton from 'material-ui/RaisedButton'
 import TextField from 'material-ui/TextField'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MenuItem from 'material-ui/MenuItem'
+import Drawer from 'material-ui/Drawer';
+
+
 
 class ViewFolder extends Component {
 
@@ -16,7 +21,8 @@ class ViewFolder extends Component {
     super(props)
     this.state = {
       clicked: false,
-      newName: ''
+      newName: '',
+      open: false
     }
     this.onFolderClick = this.onFolderClick.bind(this)
     this.renderForm = this.renderForm.bind(this)
@@ -83,10 +89,19 @@ class ViewFolder extends Component {
     })
   }
 
+  handleToggle(){ 
+    this.setState({open: !this.state.open})
+  }
+
+  handleClose(){
+    this.setState({open: false})
+  }
+
+
   render () {
     return (
       <div>
-        <AppBar title='NoteJS' className='navbar' iconClassNameRight='muidocs-icon-navigation-expand-more' iconElementRight={<form className='inputForm' onSubmit={this.onHandleSubmit}>
+        <AppBar title='NoteJS' className='navbar' onTouchTap={this.handleToggle.bind(this)} iconClassNameRight='muidocs-icon-navigation-expand-more' iconElementRight={<form className='inputForm' onSubmit={this.onHandleSubmit}>
           <TextField
             className='addInputField'
             placeholder='Create new note'
@@ -95,6 +110,19 @@ class ViewFolder extends Component {
           />
           <RaisedButton label='Submit' type='submit' />
         </form>} />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem >Change Folder Name</MenuItem>
+          <MenuItem >Unshare Folder</MenuItem>
+          <MenuItem >Delete Folder</MenuItem>
+          <MenuItem >Edit Profile</MenuItem>
+          <MenuItem >Preferences</MenuItem>
+          <MenuItem >Sign Out</MenuItem>
+        </Drawer>
         <div>{ this.renderFolder() }</div>
       </div>
       )
