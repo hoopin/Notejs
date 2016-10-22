46ad69a314fd1342bea0efd8ad46cab8397ac184
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-22 16:15:33 -0700

    [fix] - fix git conflicts

--- app/components/note/note_notetaking.js
+++ app/components/note/note_notetaking.js
@@ -3,8 +3,42 @@ import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draf
 import AppBar, {muiTheme} from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 import { connect } from 'react-redux'
+
 // let currentNoteData = convertFromRaw(JSON.parse(this.props.currentNote.content))
 
+import RaisedButton from 'material-ui/RaisedButton';
+
+const styleMap = {
+  'CODE': {
+    color: '#535353',
+    background: 'none',
+    backgroundColor: '#E4E3E4',
+    textShadow: '0 1px rgba(0, 0, 0, 0.3)',
+    fontFamily: 'monospace',
+    textAlign: 'left',
+    margin: 'none',
+    padding: '7px',
+    borderLeft: 'solid 4px #1EAAF1',
+    fontSize: '14px',
+    whiteSpace: 'pre',
+    width: '100%',
+    wordSpacing: 'normal',
+    wordBreak: 'normal',
+    wordWrap: 'normal',
+    lineHeight: 2.0,
+    hyphens: 'none'
+  },
+  'H1': {
+    fontSize: '45px'
+  },
+  'H2': {
+    fontSize: '35px'
+  },
+  'H3': {
+    fontSize: '25px'
+  }
+}
+>>>>>>> [client-feature] - style note view and add code block functionality
 
 class MyEditor extends Component {
   constructor (props) {
@@ -12,11 +46,13 @@ class MyEditor extends Component {
     this.state = { value: '', editorState: EditorState.createEmpty() }
     this.onChange = (editorState) => this.setState({editorState})
   }
+
   componentWillMount () {
     // this.props.fetchNote(this.props.params.id)
     // console.log('EDITING note id:', this.props.params.id)
     // console.log('this.props.noteData:', this.props)
   }
+
   _saveContent () {
     let noteId = this.props.idData
     let content = JSON.stringify(convertToRaw(this.state.editorState.getCurrentContent()))
@@ -30,8 +66,20 @@ class MyEditor extends Component {
     // console.log("this.pro÷ps", this.props);
     this.props.deleteNoteData(noteId)
   }
+  _onH1Click () {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'H1'))
+  }
+  _onH2Click () {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'H2'))
+  }
+  _onH3Click () {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'H3'))
+  }
   _onBoldClick () {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'))
+  }
+  _onCodeblockClick () {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'CODE'))
   }
   _onUnderlineClick () {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'))
@@ -49,13 +97,20 @@ class MyEditor extends Component {
         <div id='content' className='noteView'>
           <div id='editor'>
 
-            <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-            <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
-            <button onClick={this._onItalicClick.bind(this)}>Italic</button>
-            <button>Code Block</button>
-            <Editor placeholder='Type Here' className='editNoteBlock' editorState={this.state.editorState} onChange={this.onChange} />
-            <button onClick={this._saveContent.bind(this)}>Save</button>
-            <button onClick={this._deleteNote.bind(this)}>DELETENOTE</button>
+            <div className='stylingDiv'>
+              <button className='noteStylingButtons' onClick={this._onH1Click.bind(this)}>H1</button>
+              <button className='noteStylingButtons' onClick={this._onH2Click.bind(this)}>H2</button>
+              <button className='noteStylingButtons' onClick={this._onH3Click.bind(this)}>H3</button>
+              <button className='noteStylingButtons' onClick={this._onBoldClick.bind(this)}>B</button>
+              <button className='noteStylingButtons' onClick={this._onUnderlineClick.bind(this)}>U</button>
+              <button className='noteStylingButtons' onClick={this._onItalicClick.bind(this)}>I</button>
+              <button className='noteStylingButtons' onClick={this._onCodeblockClick.bind(this)}>Code</button>
+            </div>
+            <br /><br />
+            <Editor placeholder="What's on your mind?" className='editNoteBlock' editorState={this.state.editorState} onChange={this.onChange} customStyleMap={styleMap} />
+            <br /><br />
+            <RaisedButton className='noteBottomButtons' label='Save' onClick={this._saveContent.bind(this)} />
+            <RaisedButton className='noteBottomButtons' label='Delete Note' onClick={this._deleteNote.bind(this)} />
 
           </div>
         </div>
