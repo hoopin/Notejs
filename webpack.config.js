aaa0c1c0c36548717e08668e5b4f85d16a17de1d
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-24 12:37:59 -0700

    [fix] - edit package json and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ var config = {
         test: /\.js?/,
         include: APP_DIR,
         loader: 'babel',
-        query: { presets: ['es2015', 'react', 'stage-3'] }
+        query: { presets: ['es2015', 'react'] }
       }
     ]
   }
