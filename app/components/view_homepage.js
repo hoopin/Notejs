c7639ff468f1c727b27bb15bdae2234ed8b60efe
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-10-23 21:49:16 -0700

    [refactor] - removed extra spaces and comments

--- app/components/view_homepage.js
+++ app/components/view_homepage.js
@@ -5,41 +5,40 @@ import AppBar, {muiTheme} from 'material-ui/AppBar'
 import FlatButton from 'material-ui/FlatButton'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import MenuItem from 'material-ui/MenuItem'
-import Drawer from 'material-ui/Drawer';
+import Drawer from 'material-ui/Drawer'
 
 injectTapEventPlugin()
 
 class HomePage extends Component {
 
-
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
+  constructor (props) {
+    super(props)
+    this.state = {open: false}
   }
 
-  handleToggle(){
+  handleToggle () {
     this.setState({open: !this.state.open})
-}
+  }
 
-  handleClose(){
+  handleClose () {
     this.setState({open: false})
-}
+  }
 
   render () {
     return (
       <div>
-      <AppBar
-        title='NoteJS'
-        className='navbar'
-        iconClassNameRight='muidocs-icon-navigation-expand-more'
-        onLeftIconButtonTouchTap={this.handleToggle.bind(this)}
-        iconElementRight={
-          <span>
-            <FlatButton label='Sign in' className='authButtons' containerElement={<Link to='signin' />} />
-            <FlatButton label='Sign up' className='authButtons' containerElement={<Link to='signup' />} />
-          </span>
-        }
-      />
+        <AppBar
+          title='NoteJS'
+          className='navbar'
+          iconClassNameRight='muidocs-icon-navigation-expand-more'
+          onLeftIconButtonTouchTap={this.handleToggle.bind(this)}
+          iconElementRight={
+            <span>
+              <FlatButton label='Sign in' className='authButtons' containerElement={<Link to='signin' />} />
+              <FlatButton label='Sign up' className='authButtons' containerElement={<Link to='signup' />} />
+            </span>
+          }
+        />
         <Drawer
           docked={false}
           width={200}
@@ -53,8 +52,7 @@ class HomePage extends Component {
           <MenuItem >Contact Us</MenuItem>
         </Drawer>
         <div className='background'>
-          <Link to='folders'> render folders</Link>
-
+          <Link to='folders' className='renderFolders'>Render folders</Link>
           <div>
             <h1 className='title'><strong>NoteJS</strong></h1>
             <h3 className='tagline'>The premier note taking app for developers</h3>
