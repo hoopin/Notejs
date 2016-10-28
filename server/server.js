d492129c61fe8e3c75f436bd159712e99626617e
John Huynh <huynh.j93@gmail.com>
2016-10-28 13:54:19 -0700

    [add]-Facebook login button

--- server/server.js
+++ server/server.js
@@ -5,12 +5,42 @@ const path = require('path')
 const cors = require('cors')
 const rootRouter = require('./routers/index')
 const session = require('express-session')
+const passport = require('passport')
+const User = require('./db/db').Users
+const Strategy = require('passport-facebook').Strategy
 const PORT = 8000
+
+passport.use(new Strategy({
+		clientID: '1161007270614117',
+		clientSecret: 'f3c61e55855a23372822ab1007454d0c',
+		callbackURL: 'http://localhost:8000/login/facebook/return'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+    	console.log('hi im in the new strategy', profile);
+      User.create({
+        email: profile.displayName,
+        password: profile.id
+      });
+
+    	return cb(null,profile);
+    }
+));
+
+
+passport.serializeUser(function(user,cb){
+	cb(null,user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+	cb(null,obj);
+});
+
 
 const app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors())
 app.use(session({
   secret: 'notejs2016',
@@ -18,9 +48,24 @@ app.use(session({
   saveUninitialized: true,
   cookie: { secure: true }
 }))
+app.use(passport.initialize());
+app.use(passport.session());
+app.get('/login/facebook',
+  passport.authenticate('facebook'
+));
 
+app.get('/login/facebook/return',passport.authenticate('facebook'), function(req,res){
+	res.redirect('/folders');
+})
+// app.get('/folders',function(req,res){
+// 	res.send('i am in folderse backend');
+// })
 app.use('/', express.static(path.join(__dirname, '../public')))
 
 app.use('/api', rootRouter)
+
+app.get('*',function(req,res){
+	res.sendFile(path.join(__dirname,'../public/index.html'))
+})
 
 app.listen(PORT, () => console.log('Server running on port', PORT))
