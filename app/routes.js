8ef5c1d14d55d3e0f5ab072ae5f045a2de337757
Tim <timotheeaneumann@gmail.com>
2016-10-29 07:08:40 -0700

    [fixed]merged conflicts and added auto save feature

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,5 @@
 import React from 'react'
-<<<<<<< 078da1b531663f0256c3983ed8337c1912a89f98
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
-=======
-import { Route, IndexRoute, browserHistory } from 'react-router'
->>>>>>> [add]-adding auto-save feature and made bug fixes within note editor
 
 import App from './components/app'
 import ViewFolder from './components/view_folder'
@@ -18,7 +14,6 @@ import paymentInput from './components/view_payment_input'
 import Payment from './components/payment'
 
 export default (
-<<<<<<< 078da1b531663f0256c3983ed8337c1912a89f98
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={ViewHomePage} />
@@ -33,16 +28,4 @@ export default (
       </Route>
       <Route path='folders' component={ViewFolders} />
     </Router>
-=======
-  <Route path='/' component={App} history={browserHistory} >
-    <IndexRoute component={ViewHomePage} />
-    <Route path='notes/:id' component={ViewNoteContent} />
-    <Route path='editNote/:id' component={ViewNoteIndex} />
-    <Route path='folders/:id' component={ViewFolder} />
-    <Route path='signup' component={ViewSignUp} />
-    <Route path='signin' component={ViewSignIn} />
-    <Route path='all' component={ViewAllFiles} />
-    <Route path='folders' component={ViewFolders} />
-  </Route>
->>>>>>> [add]-adding auto-save feature and made bug fixes within note editor
 )
